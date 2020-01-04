ba522d30ed1846f926beba86c84b37bdeda5b654
moka <mokacraft@gmail.com>
2020-01-04 22:06:21 +0100

    cors

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function parse4chanJson(object, loadMore) {
 server.get("/", (req, res) => {
     // Probabbly bad practice
     // Need to specify domain instead of *
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://mokacraft.github.io");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     const website = req.query.website;
